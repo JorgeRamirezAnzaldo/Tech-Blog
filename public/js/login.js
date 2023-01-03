a6c52ee93a4d0df63c486dcf2ff6049978de09f1
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-01-03 10:13:42 -0600

    Adding comments for the login and logout js files

--- public/js/login.js
+++ public/js/login.js
@@ -1,61 +1,57 @@
-const loginForm = document.querySelector("#login-form");
-const link = document.getElementById("link");
-const button = document.getElementById("formbutton");
-const title = document.getElementById("title");
+const loginForm = document.querySelector("#login-form"); //Get the login form by its id
+const link = document.getElementById("link"); //Get the link element by its id
+const button = document.getElementById("formbutton"); //Get the button element by its id
+const title = document.getElementById("title"); //Get the title element by its id
 
+//Handler when clicking in the Login Form
 const FormHandler = async (event) => {
     event.preventDefault();
-    if (event.target.matches("#link")){
-        let linktext = link.innerText;
-        if (linktext == "Sign up instead"){
-            link.innerText = "Log in instead";
-            button.innerText = "Sign Up!";
-            title.innerText = "Sign Up";
-        } else if (linktext == "Log in instead"){
-            link.innerText = "Sign up instead";
-            button.innerText = "Log In!";
-            title.innerText = "Log In";
+    if (event.target.matches("#link")){ //Verify if the click was done in the link
+        let linktext = link.innerText; //Get the text of the link
+        if (linktext == "Sign up instead"){ //If the text of the link is equal to Sign up instead
+            link.innerText = "Log in instead"; //Change the text of the link to Log in instead
+            button.innerText = "Sign Up!"; //Change the text of the button to Sign up
+            title.innerText = "Sign Up"; //Change the text of the title to Sign up
+        } else if (linktext == "Log in instead"){ //If the text of the link is equal to Log in instead
+            link.innerText = "Sign up instead"; //Change the text of the link to Sign up instead
+            button.innerText = "Log In!"; //Change the text of the button to Log in
+            title.innerText = "Log In"; //Change the text of the title to Log in
         }
-    } else if (event.target.matches("#formbutton")){
-        console.log("Submit");
+    } else if (event.target.matches("#formbutton")){ //Verify if the click was done in the submit button
         event.preventDefault();
-        let titletext = title.innerText;
-        const username = document.querySelector('#user-input').value.trim();
-        const password = document.querySelector('#password-input').value.trim();
-        console.log(username);
-        console.log(password);
-        if (titletext == "Log In"){
-            console.log("Log In");
-            if (username && password){
-                const responselogin = await fetch('/api/users/login', {
+        let titletext = title.innerText; //Get the text of the title
+        const username = document.querySelector('#user-input').value.trim(); //Get the value of the user input element
+        const password = document.querySelector('#password-input').value.trim(); //Get the value of the password input element
+        if (titletext == "Log In"){ //If the text of the title is equal to Log in
+            if (username && password){ //Verify if username and password were provided
+                const responselogin = await fetch('/api/users/login', { //Fetch route /api/users/login with POST to log in to the application
                     method: 'POST',
-                    body: JSON.stringify({ username, password }),
+                    body: JSON.stringify({ username, password }), //Send username and password
                     headers: { 'Content-Type': 'application/json' },
                 });
             
-                if (responselogin.ok) {
-                    document.location.replace('/');
-                } else {
-                    alert('Failed to log in.');
+                if (responselogin.ok) { //If response was ok
+                    document.location.replace('/'); //Replace the document location with home
+                } else { //If response was not ok
+                    alert('Failed to log in.'); //Display an alert when the log in failed
                 }
             }
-        } else if (titletext == "Sign Up"){
-            console.log("Sign Up");
-            if (username && password){
-                const responsesignup = await fetch('/api/users', {
+        } else if (titletext == "Sign Up"){ //If the text of the title is equal to Sign up
+            if (username && password){ //Verify if username and password were provided
+                const responsesignup = await fetch('/api/users', { //Fetch route /api/users with POST to create user in database
                     method: 'POST',
-                    body: JSON.stringify({ username, password }),
+                    body: JSON.stringify({ username, password }), //Send username and password
                     headers: { 'Content-Type': 'application/json' },
                 });
             
-                if (responsesignup.ok) {
-                    document.location.replace('/');
-                } else {
-                    alert('Failed to sign up.');
+                if (responsesignup.ok) { //If response was ok
+                    document.location.replace('/'); //Replace the document location with home
+                } else { //If response was not ok
+                    alert('Failed to sign up.'); //Display an alert when the sign up failed
                 }
             }
         }
     }
 }
 
-loginForm.addEventListener('click', FormHandler);
+loginForm.addEventListener('click', FormHandler); //Create event listener for a click in the Login Form
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,15 @@
+//Function to perform logout from application
 const logout = async () => {
-    const responselogout = await fetch('/api/users/logout', {
+    const responselogout = await fetch('/api/users/logout', { //Fetch route /api/users/logout to perform logout with POST
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
   
-    if (responselogout.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to log out.');
+    if (responselogout.ok) { //If response was ok
+      document.location.replace('/'); //Change document location to home
+    } else { //If response was not ok
+      alert('Failed to log out.'); //Send an alert with failure message
     }
   };
   
-  document.querySelector('#logout').addEventListener('click', logout);
+  document.querySelector('#logout').addEventListener('click', logout); //Determine an event listener for the Logout link the in the navigation bar
