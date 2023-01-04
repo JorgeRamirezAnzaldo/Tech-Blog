d33e3d966e1216c1ce52ed5bb8310bea62297031
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-01-04 10:56:36 -0600

    Adding comments for views and helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
-module.exports = {
-    format_date: (date) => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+module.exports = { //Export funtion
+    format_date: (date) => { //Method to format the date
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${ //Create date with format month/day/year and return it
           new Date(date).getFullYear()}`;
       },
 };
