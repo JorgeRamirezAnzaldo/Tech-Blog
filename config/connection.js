b44c02fe59b377a10175e5ee87ceb7838af7bec3
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-01-08 14:01:56 -0600

    Correcting connection

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 
 //Create new connection with environment variables
 if (process.env.JAWSDB_URL){
-  Sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  Sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
