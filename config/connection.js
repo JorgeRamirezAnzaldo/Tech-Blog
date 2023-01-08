5af695e593cf70c4a68d1d11e3693f76dd117883
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-01-08 13:41:05 -0600

    Modifying connection to work with JAWSDB_URL for Heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 //Import sequelize
-const Sequelize = require('sequelize');
+let Sequelize = require('sequelize');
 //Import dotenv and configure it
 require('dotenv').config();
 
 //Create new connection with environment variables
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+if (process.env.JAWSDB_URL){
+  Sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  Sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
+
+
 
 //Export connection
 module.exports = sequelize;
